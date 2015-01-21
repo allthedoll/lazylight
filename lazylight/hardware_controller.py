b68b881c8a2b4c4e0e5c1f005022efdb03b73c53
Maxwell Pray <synthead@gmail.com>
2015-01-21 10:29:22 -0800

    Add signal to trigger ToggleRelay via Celery.  Currently, it's a placeholder.

---
+++ lazylight/hardware_controller.py
@@ -0,0 +1,2 @@
+def ToggleRelay(relay):
+  True
--- lazylight/models.py
+++ lazylight/models.py
@@ -4,6 +4,11 @@ from django.db.models import CharField
 from django.db.models import BooleanField
 from django.db.models import IntegerField
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from lazylight.hardware_controller import ToggleRelay
+
 
 class Relay(Model):
   name = CharField(default="Relay", max_length=255)
@@ -14,3 +19,7 @@ class Relay(Model):
 
   class Meta:
     db_table = "relay"
+
+@receiver(post_save, sender=Relay)
+def RelayUpdated(sender, **kwargs):
+  ToggleRelay.delay(relay=kwargs["instance"])
