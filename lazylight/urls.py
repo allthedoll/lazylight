6a1a53e84085dd532fc09ba85e7bdb06b06956f4
Maxwell Pray <synthead@gmail.com>
2015-01-21 09:57:25 -0800

    GET request to / renders "Hello World!"

--- lazylight/urls.py
+++ lazylight/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'lazylight.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns("",
+  url(r"^$", "lazylight.views.index", name="index")
 )
