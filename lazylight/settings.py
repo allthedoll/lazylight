62d6d8a1d35ffc5219ed2c623b4a9a35057fa615
Maxwell Pray <synthead@gmail.com>
2015-01-21 10:08:32 -0800

    Move local settings to an .ini file.

--- lazylight/settings.py
+++ lazylight/settings.py
@@ -1,84 +1,60 @@
-"""
-Django settings for lazylight project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from configparser import RawConfigParser
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+config = RawConfigParser()
+config.read(os.path.join(BASE_DIR, "lazylight", "settings.ini"))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+SECRET_KEY = config.get("django", "secret_key")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-#-+r&#c*yr-&it9yb13d78)mlt0h@-r2zgy2dvxq48=1@d+%8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
+# FIXME: Don't run in debug mode for production.
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = (
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.messages',
     "lazylight",
-    'django.contrib.staticfiles',
+    "django.contrib.staticfiles",
 )
 
-MIDDLEWARE_CLASSES = (
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+MIDDLEWARE_CLASSES = ()
 
-ROOT_URLCONF = 'lazylight.urls'
-
-WSGI_APPLICATION = 'lazylight.wsgi.application'
+ROOT_URLCONF = "lazylight.urls"
+WSGI_APPLICATION = "lazylight.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "mysql.connector.django",
+        "HOST": config.get("database", "host"),
+        "NAME": config.get("database", "name"),
+        "PASSWORD": config.get("database", "password"),
+        "PORT": config.get("database", "port"),
+        "USER": config.get("database", "user"),
+        "OPTIONS": {"autocommit": True}
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
